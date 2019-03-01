51e23331d9e75a61b47cdbb6b407dc058f44fc1f
Daniel Duffield <danieljduffield@gmail.com>
2019-02-28 18:15:31 -0800

    Deleted defunct actions util file and removed actions and animation utils from the index.

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,13 +1,9 @@
-const actions = require('./actions')
-const animations = require('./animations')
 const colors = require('./colors')
 const dom = require('./dom')
 const grid = require('./grid')
 const misc = require('./misc')
 
 module.exports = {
-  actions,
-  animations,
   dom,
   colors,
   grid,
