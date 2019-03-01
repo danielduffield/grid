08978df927951b9bae52e8129a4b3dfdb4a180e9
Daniel Duffield <danieljduffield@gmail.com>
2019-02-28 18:19:16 -0800

    Major refactor to nodule model to move away from timers.

--- client/models/nodule.js
+++ client/models/nodule.js
@@ -1,31 +1,60 @@
+const utils = require('../utils')
+const { ROWS, COLUMNS } = require('../state/globals')
+
 class Nodule {
-  constructor($nodule) {
+  constructor(coords) {
+    const { x, y } = coords
+    this.x = x
+    this.y = y
+
+    this.state = {}
+    this.$nodule = null
+    this.shouldRerender = false
+
+    this.update = this.update.bind(this)
+    this.createInitial$nodule = this.createInitial$nodule.bind(this)
+    this.create$nodule = this.create$nodule.bind(this)
+    this.render = this.render.bind(this)
+  }
+
+  update(key, value) {
+    this.state[key] = value
+    this.shouldRerender = true
+  }
+
+  createInitial$nodule() {
+    return utils.dom.createElement(
+      'div',
+      '',
+      { id: `${this.x}-${this.y}` },
+      {
+        height: `calc(100vh / ${ROWS})`,
+        width: `${100 / COLUMNS}%`,
+        float: 'left',
+      },
+      [],
+    )
+  }
+
+  create$nodule() {
+    const attributes = Object.keys(this.state).map(key => ({ key, value: this.state[key] }))
+    const $nodule = this.createInitial$nodule()
+    attributes.forEach(attr => utils.misc.deepSet($nodule, attr.key, attr.value))
+    return $nodule
+  }
+
+  render() {
+    let $nodule = null
+    if (!(Object.keys(this.state).length)) {
+      $nodule = this.createInitial$nodule()
+    } else if (this.shouldRerender) {
+      $nodule = this.create$nodule()
+    } else {
+      $nodule = this.$nodule
+    }
+    this.shouldRerender = false
     this.$nodule = $nodule
-
-    const [row, column] = $nodule.id.split('-')
-    this.row = row
-    this.column = column
-    this.actionTimers = []
-
-    $nodule.addEventListener('click', () => console.log(this))
-
-    this.addTimer = this.addTimer.bind(this)
-    this.clearTimers = this.clearTimers.bind(this)
-    this.isActive = this.isActive.bind(this)
-  }
-
-  isActive(propKey) {
-    return this.actionTimers.filter(timer => timer.propKey === propKey).length > 1
-  }
-
-  addTimer(timer, propKey) {
-    this.actionTimers = [...this.actionTimers, { timer, propKey }]
-  }
-
-  clearTimers(propKey) {
-    this.actionTimers.filter(timer => timer.propKey === propKey)
-      .forEach(timer => clearTimeout(timer.timer))
-    this.actionTimers = this.actionTimers.filter(timer => timer.propKey !== propKey)
+    return this.$nodule
   }
 }
 
