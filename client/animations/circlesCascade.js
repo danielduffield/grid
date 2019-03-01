de0ff8a2b658b7d82c8f224671901fb35073539b
Daniel Duffield <danieljduffield@gmail.com>
2019-02-28 18:16:54 -0800

    Deleted now defunct animation function files.

--- client/animations/circlesCascade.js
+++
@@ -1,9 +0,0 @@
-const utils = require('../utils')
-const { Timeframe } = require('../models')
-const { multiplyInnerColors } = utils.colors
-
-const circlesCascade = (nodule, timeframe) => {
-  const sizes = utils.misc.generateArrayOfNums(11).map(num => `${num * 10}%`)
-}
-
-module.exports = circlesCascade
--- client/animations/textCascade.js
+++
@@ -1,9 +0,0 @@
-const utils = require('../utils')
-const { Timeframe } = require('../models')
-const { multiplyInnerColors } = utils.colors
-
-const textCascade = (nodule, timeframe) => {
-  const text = 'All work and no play makes Jack a dull boy.'
-}
-
-module.exports = textCascade
