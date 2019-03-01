bb75cd74043610575a334b02d0eae7a0e7bccb08
Daniel Duffield <danieljduffield@gmail.com>
2019-02-28 18:24:09 -0800

    Removed the action model from the models index.

--- client/models/index.js
+++ client/models/index.js
@@ -1,10 +1,8 @@
-const Action = require('./action')
 const Matrix = require('./matrix')
 const Nodule = require('./nodule')
 const Timeframe = require('./timeframe')
 
 module.exports = {
-  Action,
   Matrix,
   Nodule,
   Timeframe,
